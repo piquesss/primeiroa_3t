c5168c34a45847eff2b99c2a86f7101994d2f434
piquesss <108939994+piquesss@users.noreply.github.com>
2022-11-04 11:11:40 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,8 +1,8 @@
-let tabuada = 22;
+let tabuada = 1;
 let nome = "Rodrigo";
 
 function escreva() {
-    document.write("Tabuada do " + tabuada + "<br>")
+    document.write("tabuada do " + tabuada + "<br>")
     document.write(tabuada + " x 1 = " + (tabuada * 1) + "<br>");
     document.write(tabuada + " x 2 = " + (tabuada * 2) + "<br>");
     document.write(tabuada + " x 3 = " + (tabuada * 3) + "<br>");
@@ -13,8 +13,7 @@ function escreva() {
     document.write(tabuada + " x 8 = " + (tabuada * 8) + "<br>");
     document.write(tabuada + " x 9 = " + (tabuada * 9) + "<br>");
     document.write(tabuada + " x 10 = " + (tabuada * 10) + "<br><br>");
-    document.write("Feito por " + Rodrigo);
-
+    document.write("feito por " + nome);
 }
 
 function minhaTabuada() {
@@ -24,72 +23,78 @@ function minhaTabuada() {
             document.write(i + " x " + j + " = " + (j * i) + "<br>");
         }
         document.write("<br>");
-
-
     }
 }
 
 function quadrado() {
-    for (let i = 2; i <= 20; i++) {
-        document.write("O quadrado de " + i + " é " + (i * i) + "<br>")
+    for (let i = 1; i <= 499; i++) {
+        document.write("O quadrado de " + i + " é " + (i * i) + "<br>");
     }
 }
 function moeda(atual) {
     return atual.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+
 }
+
 function calcula() {
-    let c = document.getElementById("valor").value = "";
+    let c = document.getElementById("valor").value;
     let j = document.getElementById("juros").value;
     let t = document.getElementById("meses").value;
 
-    if (!Number(c)) {
-        alert("O valor do capital deve ser numérico.");
-        document.getElementById("valor").value = "";
-        let j = document.getElementById("valor").focus();
-        document.getElementById("juros").value = "";
-        return
-    }
-    if (!Number(j)) {
-        alert("O valor do juros deve ser numérico.");
-        document.getElementById("juros").value = "";
-        document.getElementById("juros").focus();
-        return
+if (!Number(c)) {
+    alert("o valor do capital deve ser numerico")
+    document.getElementById("valor").value = "";
+    document.getElementById("valor").focus();
+    return
 }
-        if (!Number(t)) {
-            alert("a quantidade de meses deve ser numérico.");
-            document.getElementById("juros").value = "";
-            document.getElementById("juros").focus();
-            return
-            }
-            let r = c;
-            for(let m = 1; m <= t; m++){
-                r = c * (1 + (j / 100));
-                r = r; 
-                document.write("Mẽs " + m + "- valor: " + moeda(r)+"<br>"
-            }
-            
+if (!Number(j)) {
+    alert("o valor do juros deve ser numerico")
+    document.getElementById("juros").value = "";
+    document.getElementById("juros").focus();
+    return
+}
+if (!Number(t)) {
+    alert("a quantidade de meses deve ser numerico")
+    document.getElementById("meses").value = "";
+    document.getElementById("meses").focus();
+    return
+}
+let r = c;
+for (let m = 1; m <= t; m++) {
+    r = c * (1 + (j / 100));
+    c = r;
+    texto += m + " - : " + moeda(r) + "<br>"
+    document.write("Mes " + m + " - valor: " + moeda(r) + "<br>");
+}
+document.getElementById("total").innerHTML = texto;
+document.getElementById("total").innerHTML = moeda;
 
-        
+document.write("resultado: " + moeda(r));
+}
 
-        
-    
+let op = "";
+function operacao(ope) {
+    op = ope;
+}
+function calcule() {
+    let v1 = document.getElementById("v1").value;
+    let v2 = document.getElementById("v2").value;
+    let r = 0;
 
-       
-        document.write("Resultado: " + moeda(r));
+    if (op == "+") {
+        r = Number(v1) + Number(v2);
     }
-    let op = "";
-    function operação(ope) {
-        op = ope;
-
+    if (op == "-") {
+        r = Number(v1) - Number(v2);
     }
-    function calcule() {
-        let v1 = document.getElementById("v1").value;
-        let v2 = document.getElementById("v2").value;
-        let r = 0;
-
-        if (op == "+") {
-            r = Number(v1) + Number(v2);
-        }
-
+    if (op == "*") {
+        r = Number(v1) * Number(v2);
     }
+    if (op == "/") {
+        r = Number(v1) / Number(v2);
+    }
+
+
     document.getElementById("resultado").innerHTML = r;
+
+}
